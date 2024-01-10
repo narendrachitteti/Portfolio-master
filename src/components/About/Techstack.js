a011292cb9ea0fe0307d058cf0385aca9e498378
Narendra Chitteti <122769756+narendrachitteti@users.noreply.github.com>
2024-01-10 16:46:02 +0530

    hii

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,23 +1,17 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiPython,
   DiGit,
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
   SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import billing from "../../Assets/Projects/billing.png";
 import hospital from "../../Assets/Projects/hospital.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import login12 from "../../Assets/Projects/login12.png";
 
 function Projects() {
